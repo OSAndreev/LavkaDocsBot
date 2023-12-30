bc265471556c7963d461ae7a4568fdb9be5cf738
OSAndreev <andreevos22@mail.ru>
2023-12-31 03:01:17 +0400

    Connected to Yandex Mail

--- handlers/common.py
+++ handlers/common.py
@@ -1,8 +1,11 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from aiogram import Router, F
+from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
-from telethon.sync import TelegramClient
+import imaplib
+import email
+import base64
+from bs4 import BeautifulSoup
 from main import bot
 
 
@@ -10,65 +13,116 @@ from main import bot
 scheduler = AsyncIOScheduler()
 router = Router()
 form = []
-forms_info = []
+# Ссылки на формы по порядку
 form_links = ['https://forms.yandex.ru/u/6574ffa090fa7b5ff767f78a/', 'https://forms.yandex.ru/u/6575008d43f74f601e0891dc/', 'https://forms.yandex.ru/u/657500b4eb614663189bd7b2/']
+mail = imaplib.IMAP4_SSL('imap.yandex.ru')
+mail.login('akelfa@yandex.ru', 'sshjgumsnkahaifk')
+
 
 @router.message(Command('get_docs'))
 async def get_docs_command(message: Message, state):
-    global chat_id
-    global username
-    chat_id = message.chat.id
+    # Очищаем информацию по формам на всякий случай
+    await state.update_data(forms_info=[])
+    chat_id = str(message.chat.id)
+    await state.update_data(chat_id=chat_id)
     try:
         # Получаем username из команды
         username = message.text.split(' ')[1]
-
+        await state.update_data(username=username)
         # Отправляем сообщение с приветствием
         await message.answer(f"{username}, заполните, пожалуйста документы. Вот ссылка: https://forms.yandex.ru/u/6574ffa090fa7b5ff767f78a/")
-        scheduler.add_job(get_message, "interval", seconds=10)
-        scheduler.add_job(remind_form, "interval", minutes=1, args=(f"{username}, заполните, пожалуйста документы. Вот ссылка: https://forms.yandex.ru/u/6574ffa090fa7b5ff767f78a/)", ), id='form1')
+        scheduler.add_job(get_message, "interval", seconds=10, args=(state, ), id='parser' + chat_id)    # Запускаем парсинг ответов с почты
+        # Запускаем регулярные напоминания в чат
+        scheduler.add_job(remind_form, "interval", minutes=1, args=(f"{username}, заполните, пожалуйста документы. Вот ссылка: https://forms.yandex.ru/u/6574ffa090fa7b5ff767f78a/)", state ),
+                          id='form' + '&' + chat_id + '&' + '1')
     except IndexError:
         # Если не указан username, отправляем сообщение об ошибке
         await message.answer("Пожалуйста, укажите username после команды /get_docs")
 
-async def send_second_form(form_num):
-    global chat_id
-    global bot
+
+# Отправка сообщения со ссылкой на следующую форму
+async def send_second_form(form_num, state):
+    user_data = await state.get_data()
+    chat_id = user_data['chat_id']
+    username = user_data['username']
     global form_links
     await bot.send_message(chat_id, f"Поздравляем! Вы прошли ещё один этап квеста. {username}, заполните, пожалуйста документы. Вот ссылка: {form_links[form_num]}")
-    scheduler.add_job(remind_form,"interval", minutes=1, args=(f"{username}, заполните, пожалуйста документы. Вот ссылка: {form_links[form_num]}", ),
-                      id=f'form{form_num + 1}')
+    scheduler.add_job(remind_form, "interval", minutes=1, args=(f"{username}, заполните, пожалуйста документы. Вот ссылка: {form_links[form_num]}", state),
+                      id=f'form&{chat_id}&{form_num + 1}')
 
 
-async def get_message():
-    global forms_info
-    async with TelegramClient('lavka_bot', '22526632', 'a2adec3a58f237733ab5521b5f75337b') as client:
-        await client.start()
-        chat = await client.get_entity(-4072882592)
-        last_message = await client.get_messages(chat, limit=1)
-        last_message = last_message[0]
-        if (not forms_info or forms_info[-1].id != last_message.id) and 'форма' in last_message.text.lower():
-            forms_info.append(last_message)
-            scheduler.remove_job('form' + str(len(forms_info)))
-            if len(forms_info) == 3:
-                await send_forms_info()
-                scheduler.remove_all_jobs()
+# Функция, которая парсит новые письма с информацией о заполнении почты
+# Если находит новое письмо, кладёт ответы в form_info
+async def get_message(state):
+    user_data = await state.get_data()
+    forms_info = user_data['forms_info']
+    mail.list()
+    mail.select("inbox")
+    result, data = mail.search(None, "ALL")
+    ids = data[0]
+    id_list = ids.split()
+    if len(id_list) > 0:
+        latest_email_id = id_list[-1]
+        print(latest_email_id)
+        result, data = mail.fetch(latest_email_id, "(RFC822)")
+        raw_email = data[0][1]
+        raw_email_string = raw_email.decode('utf-8')
+        email_message = email.message_from_string(raw_email_string)
+        subject = email_message['Subject'].split('?')[3]
+        subject = base64.b64decode(subject).decode('utf-8')
+        if 'form' in subject:
+            # Получение текста письма
+            body = None
+            if email_message.is_multipart():
+                for part in email_message.walk():
+                    if part.get_content_maintype() == 'text':
+                        body = BeautifulSoup(base64.b64decode(part.get_payload()).decode(),
+                                             'html.parser').body.get_text()
+
+                        body = str(body)
+                        break
             else:
-                await send_second_form(len(forms_info))
+                body = email_message.get_payload(decode=True).decode('utf-8')
+            answer_id, answers = body.split('answer_id:')[1].split(',answer_data:')
+            answer_data = dict()
+            for pair in answers.split('\n\n')[:-2]:
+                key, value = pair.split(':\n')
+                answer_data[key] = value
+            if '@' + answer_data['username'] == user_data['username'] and \
+                    (not forms_info or forms_info[-1]['id'] != answer_id):
+                forms_info.append({'data': answer_data, 'id': answer_id})
+                await state.update_data(forms_info=forms_info)
+                scheduler.remove_job('form' + '&' + user_data['chat_id'] + '&' + str(len(forms_info)))
+                mail.store(latest_email_id, '+FLAGS', '\\Deleted')
+                mail.expunge()
+                # Если все три формы заполнены, отправляем информацию по ним и выгружаем в чат
+                if len(forms_info) == 3:
+
+                    await send_forms_info(state)
+                    scheduler.remove_job('parser' + user_data['chat_id'])
+                else:
+                    await send_second_form(len(forms_info), state)
+
 
     print(forms_info)
 
 
-async def send_forms_info():
+# Отправляет ответы на формы в чат
+async def send_forms_info(state):
     global bot
-    global forms_info
+    user_data = await state.get_data()
+    forms_info = user_data['forms_info']
+    user_data = await state.get_data()
+    chat_id = user_data['chat_id']
     await bot.send_message(chat_id, "Квест пройден! Вот ваши ответы:")
     for form_info in forms_info:
-        await bot.send_message(chat_id, form_info.text)
+        await bot.send_message(chat_id, '\n'.join([key + ':' + form_info['data'][key]
+                                                   for key in form_info['data']]))
 
 
-async def remind_form(text):
+# Отправляет сообщение с указанным текстом в чат
+async def remind_form(text, state):
     global bot
+    user_data = await state.get_data()
+    chat_id = user_data['chat_id']
     await bot.send_message(chat_id, text)
-# @router.message(Command('get_chat_id'))
-# async def get_chat_id(message):
-#     await message.answer(str(message.chat.id))
